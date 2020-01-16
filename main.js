f647634d0dad65c1dc051bea858c92a7be9f7129
Andrea <andreademarco91@gmail.com>
2020-01-16 15:39:23 +0100

    TEST FADE IN/FADE OUT

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 var open = $('i.fa-bars'); //variabile tasto apertura
 var close = $('i.fa-times') //variabile tasto chiusura
+
 // FUNZIONE APERTURA
+// open.click(
+//   function(){
+//     $('div.hamburger-menu').show(500)
+//   }
+// );
+// // FUNZIONE CHIUSURA
+// close.click(
+//   function(){
+//     $('div.hamburger-menu').hide(500)
+//   }
+// );
+
+// FUNZIONE APERTURA CON FADE IN
 open.click(
   function(){
-    $('div.hamburger-menu').show(500)
+    $('div.hamburger-menu').fadeIn(500)
   }
 );
-// FUNZIONE CHIUSURA
+// FUNZIONE CHIUSURA CON FADE OUT
 close.click(
   function(){
-    $('div.hamburger-menu').hide(500)
+    $('div.hamburger-menu').fadeOut(500)
   }
 );
